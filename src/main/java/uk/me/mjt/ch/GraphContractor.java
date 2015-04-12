c15be6d34ef14f33f9b46b8a37d5028a3b59a143
mtandy <mtandy@mtandy-desktop>
2015-04-12 23:12:23 +0100

    Extra logging, to check what takes the time etc.

--- src/main/java/uk/me/mjt/ch/GraphContractor.java
+++ src/main/java/uk/me/mjt/ch/GraphContractor.java
@@ -2,11 +2,17 @@ package uk.me.mjt.ch;
 
 import uk.me.mjt.ch.Dijkstra.Direction;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
  *
@@ -15,7 +21,12 @@ import java.util.TreeMap;
 public class GraphContractor {
     private final HashMap<Long,Node> allNodes;
     private long maxEdgeId;
-
+    
+    private long findShortcutsCalls = 0;
+    private long nodePreContractChecks = 0;
+    private long nodePreContractChecksPassed = 0;
+    private long millisSpentOnContractionOrdering = 0;
+    
     public GraphContractor(HashMap<Long,Node> allNodes) {
         this.allNodes = allNodes;
         this.maxEdgeId = 4294967296L;
@@ -40,6 +51,7 @@ public class GraphContractor {
     }
 
     public ArrayList<DirectedEdge> findShortcuts(Node n) {
+        findShortcutsCalls++;
         ArrayList<DirectedEdge> shortcuts = new ArrayList<DirectedEdge>();
 
         HashSet<Node> destinationNodes = new HashSet<Node>();
@@ -51,28 +63,28 @@ public class GraphContractor {
                     maxOutDist = outgoing.distance;
             }
         }
-
+        
         for (DirectedEdge incoming : n.edgesTo) {
             Node startNode = incoming.from;
             if (startNode.isContracted())
                 continue;
 
             List<DijkstraSolution> routed = Dijkstra.dijkstrasAlgorithm(allNodes,
-                                                    startNode,
-                                                    new HashSet<Node>(destinationNodes),
-                                                    incoming.distance+maxOutDist,
-                                                    Direction.FORWARDS);
+                    startNode,
+                    new HashSet<Node>(destinationNodes),
+                    incoming.distance+maxOutDist,
+                    Direction.FORWARDS);
 
             for (DijkstraSolution ds : routed) {
                 //System.out.print(ds + ", " + ds.totalDistance);
                 if (ds.nodes.size() == 3 && ds.nodes.get(1)==n) {
                     //System.out.println("  Shortcut " + ds + ", " + ds.totalDistance);
                     shortcuts.add(new DirectedEdge(maxEdgeId++,
-                                                    ds.nodes.getFirst(),
-                                                    ds.nodes.getLast(),
-                                                    ds.totalDistance,
-                                                    ds.edges.get(0),
-                                                    ds.edges.get(1)));
+                            ds.nodes.getFirst(),
+                            ds.nodes.getLast(),
+                            ds.totalDistance,
+                            ds.edges.get(0),
+                            ds.edges.get(1)));
                 } else {
                     //System.out.println();
                 }
@@ -86,31 +98,51 @@ public class GraphContractor {
     TreeMap<ContractionOrdering,Node> contractionOrder = new TreeMap<ContractionOrdering,Node>();
 
     public void initialiseContractionOrder() {
+        long orderingStart = System.currentTimeMillis();
+        contractionOrder.clear();
         for (Node n : allNodes.values()) {
             if (n.contractionAllowed && !n.isContracted()) {
                 contractionOrder.put(new ContractionOrdering(n,getBalanceOfEdgesRemoved(n)), n);
             }
         }
+        long orderingDuration = System.currentTimeMillis()-orderingStart;
+        millisSpentOnContractionOrdering += orderingDuration;
+        System.out.println("Generated contraction order for " + contractionOrder.size() + 
+                " nodes in " + orderingDuration + " ms.");
     }
     
-    public void reinitialiseContractionOrder() {
-        System.out.println("Reinitialising contraction order...");
-        ArrayList<Node> priorSet = new ArrayList<>(contractionOrder.values());
-        contractionOrder.clear();
-        for (Node n : priorSet) {
-            if (n.contractionAllowed && !n.isContracted()) {
-                contractionOrder.put(new ContractionOrdering(n,getBalanceOfEdgesRemoved(n)), n);
-            }
-        }
-    }
-
     public void contractAll() {
         int contractionProgress = 1;
+        
+        long startTime = System.currentTimeMillis();
+        long recentTime = System.currentTimeMillis();
+        long recentPreContractChecks = 0;
+        long recentPreContractChecksPassed = 0;
+        long recentOrderingMillis = 0;
 
         Node n;
-        while ((n = lazyContractNextNode(contractionProgress++, true)) != null) {
+        while ((n = lazyContractNextNode(contractionProgress++, false)) != null) {
             //System.out.println("Contracted " + n);
+            if (contractionOrder.size() % 10000 == 0) {
+                long now = System.currentTimeMillis();
+                long runTimeSoFar = now-startTime;
+                long orderingTimeThisRun = millisSpentOnContractionOrdering-recentOrderingMillis;
+                float checksPerPass = (float)(nodePreContractChecks-recentPreContractChecks) / (float)(nodePreContractChecksPassed-recentPreContractChecksPassed);
+                System.out.println(runTimeSoFar+"," + contractionOrder.size() + 
+                        "," + (now-recentTime) + "," + orderingTimeThisRun + 
+                        "," + checksPerPass);
+                recentTime=now;
+                recentPreContractChecks=nodePreContractChecks;
+                recentPreContractChecksPassed=nodePreContractChecksPassed;
+                recentOrderingMillis=millisSpentOnContractionOrdering;
+            }
         }
+       
+        System.out.println("findShortcutsCalls: "+findShortcutsCalls);
+        System.out.println("nodePreContractChecks: "+nodePreContractChecks);
+        System.out.println("nodePreContractChecksPassed: "+nodePreContractChecksPassed);
+        System.out.println("millisSpentOnContractionOrdering: " + millisSpentOnContractionOrdering);
+        
     }
 
     private Node lazyContractNextNode(int contractionProgress, boolean includeUnprofitable) {
@@ -121,6 +153,7 @@ public class GraphContractor {
             Node n = next.getValue();
 
             ArrayList<DirectedEdge> shortcuts = findShortcuts(n);
+            nodePreContractChecks++;
             
             int balanceOfEdgesRemoved = getEdgeRemovedCount(n)-shortcuts.size();
             boolean profitable = balanceOfEdgesRemoved > -30; // OK, so our threshold for 'profitable' has a bit of margin on it.
@@ -135,6 +168,7 @@ public class GraphContractor {
                     // If the ContractionOrdering is unchanged, or has changed but 
                     // not enough to move this node off the top spot, contract.
                     contractNode(n,contractionProgress,shortcuts);
+                    nodePreContractChecksPassed++;
                     return n;
                 } else {
                     // Otherwise
@@ -143,7 +177,8 @@ public class GraphContractor {
                 
             } else if (!recentlySorted) {
                 contractionOrder.put(oldOrder, n);
-                reinitialiseContractionOrder();
+                System.out.println("Reinitialising contraction order...");
+                initialiseContractionOrder();
                 recentlySorted = true;
             } else if (!includeUnprofitable) {
                 System.out.println("Contraction became unprofitable with " + contractionOrder.size() + " nodes remaining.");
@@ -161,7 +196,7 @@ public class GraphContractor {
         }
         return maxContractionDepth;
     }
-
+    
     private class ContractionOrdering implements Comparable<ContractionOrdering> {
         final int edgeCountReduction;
         final int contractionDepth;
