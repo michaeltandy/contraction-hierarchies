5b27af92bea4910f8d5b75876d131d4c65cb541d
mtandy <mtandy@mtandy-desktop>
2015-06-17 22:55:04 +0100

    Fix unit test, remove usage of uncommitted file.

--- src/test/java/uk/me/mjt/ch/GraphContractorTest.java
+++ src/test/java/uk/me/mjt/ch/GraphContractorTest.java
@@ -15,17 +15,13 @@ public class GraphContractorTest {
         
         System.out.println(GeoJson.allLinks(graph.values()));
         System.out.println("\n\n\n");
-        System.out.println(ContractionJson.allLinks(graph.values()));
-        System.out.println("\n\n\n");
         
         GraphContractor instance = new GraphContractor(graph);
         instance.initialiseContractionOrder();
         instance.contractAll();
         
-        System.out.println("\n\nasdfqwer\n\n\n\n");
-        
         Node startNode = graph.get(1L);
-        Node endNode = graph.get(78L);
+        Node endNode = graph.get(18L);
             
         DijkstraSolution contracted = Dijkstra.contractedGraphDijkstra(graph, startNode, endNode);
         System.out.println("Contraction: "+contracted);
