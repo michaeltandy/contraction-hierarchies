8084a90cdb3010e7c8347452e5d2d46a29f8f408
mtandy <mtandy@mtandy-desktop>
2015-08-09 02:13:52 +0100

    More logging on performance test script.

--- performance-test.sh
+++ performance-test.sh
@@ -5,6 +5,8 @@
 #14.04 LTS & instance store & PV: ami-6a3f7e1d
 #14.04 LTS & instance store & HVM: ami-7842030f <-- Supports r3.large (15gb, 4 cent spot), r3.xlarge (30gb, 7 cent spot)
 
+#sudo shutdown -h 50 &
+
 sudo chown ubuntu:ubuntu /mnt/
 echo 'started' > /mnt/runtimes.txt
 date >> /mnt/runtimes.txt
@@ -14,23 +16,25 @@ sudo apt-get install -y openjdk-7-jdk awscli maven git
 echo 'apt-get complete' >> /mnt/runtimes.txt
 date >> /mnt/runtimes.txt
 
-cd 
-
 mkdir /mnt/ch
 cd /mnt/ch
 
-aws --region=us-west-1 s3 cp s3://ch-test-mjt/great-britain-new-contracted-accessonly.tar.gz .
-tar -xvf great-britain-new-contracted-accessonly.tar.gz
+aws --region=us-west-1 s3 cp s3://ch-test-mjt/great-britain-new-contracted-accessonly-v2.tar.gz .
+tar -xvf great-britain-new-contracted-accessonly-v2.tar.gz
 
 git clone https://github.com/michaeltandy/contraction-hierarchies.git
 
 cd contraction-hierarchies
 #git reset --hard aa0773713aec296f460dda1610cf7a1cb8689b09 # To test a particular git revision
 
+mvn --version
+mvn help:effective-pom
+mvn help:system
+
 mvn clean install
 ch_git_rev=`git rev-parse master`
 
-log -1 >> /mnt/runtimes.txt
+git log -1 >> /mnt/runtimes.txt
 echo 'got map and compiled code' >> /mnt/runtimes.txt
 date >> /mnt/runtimes.txt
 
@@ -42,11 +46,11 @@ instance_id=`curl http://169.254.169.254/latest/meta-data/instance-id`
 
 for i in `seq 1 3`;
 do
-    java -cp /mnt/ch/contraction-hierarchies/target/ch-1.0-SNAPSHOT.jar -Xmx28g -Xms28g -XX:GCTimeLimit=60 uk.me.mjt.ch.BenchmarkUk | tee LoadAndPathUk-$i.txt
+    java -cp "/mnt/ch/contraction-hierarchies/target/classes/:/mnt/ch/contraction-hierarchies/target/ch-1.0-SNAPSHOT.jar" -Xmx28g -Xms28g -XX:GCTimeLimit=60 uk.me.mjt.ch.BenchmarkUk | tee LoadAndPathUk-$i.txt
     aws --region=us-west-1 s3 cp LoadAndPathUk-$i.txt s3://ch-test-mjt/$ch_git_rev/$instance_type/$instance_id/
     uncached_pathing_time=`cat LoadAndPathUk-$i.txt | grep 'repetitions uncached pathing' | sed 's/.*in //' | sed 's/ ms.//'`
     cached_pathing_time=`cat LoadAndPathUk-$i.txt | grep 'repetitions cached pathing' | sed 's/.*in //' | sed 's/ ms.//'`
-    curl "https://docs.google.com/forms/d/1xFOZk3D1wnIjB0N3bhNIirppMSug1qJChYbyA4JGAf0/formResponse?ifq&entry.1150050082=$instance_id&entry.1477423851=$instance_type&entry.592766186=$ch_git_rev&entry.915449080=$uncached_pathing_time&entry.884534640=$cached_pathing_time&submit=Submit"
+    curl "https://docs.google.com/forms/d/1xFOZk3D1wnIjB0N3bhNIirppMSug1qJChYbyA4JGAf0/formResponse?ifq&entry.1150050082=$instance_id&entry.1477423851=$instance_type&entry.592766186=$ch_git_rev&entry.915449080=$uncached_pathing_time&entry.884534640=$cached_pathing_time&submit=Submit" > curl-output.txt
 done
 
 echo 'test complete, shutting down' >> /mnt/runtimes.txt
@@ -65,6 +69,7 @@ date >> /mnt/runtimes.txt
 #date >> /mnt/runtimes.txt
 
 aws --region=us-west-1 s3 cp /mnt/runtimes.txt s3://ch-test-mjt/$ch_git_rev/$instance_type/$instance_id/
+aws --region=us-west-1 s3 cp /var/log/cloud-init-output.log s3://ch-test-mjt/$ch_git_rev/$instance_type/$instance_id/
 
-sudo shutdown -h now
+sudo shutdown -h 10
 
