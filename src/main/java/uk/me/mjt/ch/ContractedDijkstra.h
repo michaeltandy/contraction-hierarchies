9a065582c38be2dc656c6366f261e3900e2406f1
mtandy <mtandy@mtandy-desktop>
2015-10-22 21:36:27 +0100

    Modified c implementation, possibly improving performance by reducing branching. Let's test!

--- src/main/java/uk/me/mjt/ch/ContractedDijkstra.h
+++ src/main/java/uk/me/mjt/ch/ContractedDijkstra.h
@@ -1,46 +1,40 @@
 #include <climits>
 
-int* getCommonEntryIndicesJni(int* a, int* b, int* aTimes, int* bTimes, int aLength, int bLength) {
-    int* result = new int[2];
-    result[0]=-1;
-    result[1]=-1;
+int32_t* getCommonEntryIndicesJni(int32_t* a, int32_t* b, int32_t* aTimes, int32_t* bTimes, int32_t aLength, int32_t bLength) {
+    int32_t* result = new int[2];
+    
     if (aLength==0 || bLength==0) {
+        result[0]=-1;
+        result[1]=-1;
         return result;
     }
 
-    int shortestTime = INT_MAX;
-    int aIdx = 0;
-    int bIdx = 0;
-    int aValue = a[aIdx];
-    int bValue = b[bIdx];
+    int32_t shortestTime = INT_MAX;
+    int32_t aIdx = 0;
+    int32_t bIdx = 0;
+    
+    int32_t shortestA = -1;
+    int32_t shortestB = -1;
 
     while (aIdx<aLength && bIdx<bLength) {
-        
-        if (aValue==bValue) {
-            int aTime = aTimes[aIdx];
-            int bTime = bTimes[bIdx];
-            int thisTime = aTime+bTime;
+        int32_t aValue = a[aIdx];
+        int32_t bValue = b[bIdx];
+        int32_t aTime = aTimes[aIdx];
+        int32_t bTime = bTimes[bIdx];
+        int32_t thisTime = aTime+bTime;
 
-            if (thisTime < shortestTime) {
-                shortestTime = thisTime;
-                result[0]=aIdx;
-                result[1]=bIdx;
-            }
-                
-            bIdx++;
-            aIdx++;
-            aValue = a[aIdx];
-            bValue = b[bIdx];
-        } else if (aValue > bValue) {
-            bIdx++;
-            bValue = b[bIdx];
-        } else {
-            aIdx++;
-            aValue = a[aIdx];
-        }
+        bool improvement=(aValue==bValue && thisTime < shortestTime);
+        shortestA = (improvement?aIdx:shortestA);
+        shortestB = (improvement?bIdx:shortestB);
+        shortestTime = (improvement?thisTime:shortestTime);
+
+        aIdx = (aValue<=bValue ? aIdx+1 : aIdx);
+        bIdx = (aValue>=bValue ? bIdx+1 : bIdx);
     }
 
 
+    result[0]=shortestA;
+    result[1]=shortestB;
     return result;
 }
 
