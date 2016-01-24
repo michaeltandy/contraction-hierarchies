42153740e6003b302a98f11f380623efb51a4abf
mtandy <mtandy@mtandy-desktop>
2016-01-24 21:20:48 +0000

    Remove disabled test code that only ran, to improve code coverage.

--- src/main/java/uk/me/mjt/ch/BidirectionalTreeMap.java
+++ src/main/java/uk/me/mjt/ch/BidirectionalTreeMap.java
@@ -9,9 +9,6 @@ import java.util.NavigableMap;
 import java.util.TreeMap;
 
 public class BidirectionalTreeMap<K extends Comparable<K>,V> {
-    private int mutationCounter = 0;
-    private static final int VALIDATION_PERIOD_MUTATIONS = 0;
-    
     private final TreeMap<K,V> forwardMap = new TreeMap<>();
     private final HashMap<V,K> reverseMap = new HashMap<>();
     
@@ -22,7 +19,6 @@ public class BidirectionalTreeMap<K extends Comparable<K>,V> {
     public void clear() {
         forwardMap.clear();
         reverseMap.clear();
-        checkIfInteresting(null,null);
     }
 
     public void put(K key, V value) {
@@ -45,8 +41,6 @@ public class BidirectionalTreeMap<K extends Comparable<K>,V> {
         if (!forwardMap.containsKey(key) || !reverseMap.containsKey(value)) {
             throw new IllegalArgumentException("Add-retrieve failed?");
         }
-        
-        checkIfInteresting(key,value);
     }
 
     public boolean isEmpty() {
@@ -61,20 +55,10 @@ public class BidirectionalTreeMap<K extends Comparable<K>,V> {
         Map.Entry<K,V> result = forwardMap.pollFirstEntry();
         if (result != null) {
             reverseMap.remove(result.getValue());
-            checkIfInteresting(result.getKey(),result.getValue());
         }
         return result;
     }
 
-    /*public Map.Entry<K,V> pollLastEntry() {
-        Map.Entry<K,V> result = forwardMap.pollLastEntry();
-        if (result != null) {
-            reverseMap.remove(result.getValue());
-        }
-        if (validateEverything) validateMaps();
-        return result;
-    }*/
-    
     public K keyForValue(V value) {
         return reverseMap.get(value);
     }
@@ -87,7 +71,6 @@ public class BidirectionalTreeMap<K extends Comparable<K>,V> {
         V value = forwardMap.remove(key);
         reverseMap.remove(value);
         
-        checkIfInteresting(key,value);
         return value;
     }
     
@@ -107,41 +90,6 @@ public class BidirectionalTreeMap<K extends Comparable<K>,V> {
             sb.append(entry.getKey()).append("->").append(entry.getValue()).append("\n");
         }
         return sb.toString();
-    }
-    
-    private void checkIfInteresting(K key, V value) {
-        mutationCounter++;
-        if ( VALIDATION_PERIOD_MUTATIONS > 0 && mutationCounter%VALIDATION_PERIOD_MUTATIONS==0 ) {
-            validateMaps();
-        }
-    }
-    
-    private void validateMaps() {
-        if (forwardMap.size() != reverseMap.size()) {
-            throw new IllegalStateException("Maps somehow became non-equal? "
-                    + "Sizes don't match. Mutation Counter:" + mutationCounter);
-        }
-        
-        for (Map.Entry<K,V> e : forwardMap.entrySet()) {
-            K k1 = e.getKey();
-            V v1 = k1==null?null:forwardMap.get(k1);
-            K k2 = v1==null?null:reverseMap.get(v1);
-            V v2 = k2==null?null:forwardMap.get(k2);
-            
-            if (k1==null || k2==null || v1==null || v2==null) {
-                //System.out.println(forwardMap.getPathToKey(k1));
-                //System.out.println(forwardMap.getPathFromEntry(e));
-                throw new IllegalStateException("Maps somehow became non-equal?"
-                        + " Mutation Counter:" + mutationCounter);
-            }
-            
-            if (!k1.equals(k2) || !v1.equals(v2)) {
-                throw new IllegalStateException("Maps somehow became non-equal?"
-                        + " Mutation Counter:" + mutationCounter);
-            }
-        }
-        
-        
     }
     
     public String toString() {
