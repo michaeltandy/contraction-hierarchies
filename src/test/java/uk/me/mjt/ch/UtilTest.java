7b0542c0071ba43fbf93b685f16f57825c898ac6
mtandy <mtandy@mtandy-desktop>
2016-01-24 21:25:16 +0000

    Improve test coverage of util class.

--- src/test/java/uk/me/mjt/ch/UtilTest.java
+++ src/test/java/uk/me/mjt/ch/UtilTest.java
@@ -1,6 +1,5 @@
 package uk.me.mjt.ch;
 
-import java.util.HashMap;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -14,12 +13,21 @@ public class UtilTest {
         System.out.println("deepEquals");
         MapData a = MakeTestData.makeSimpleThreeEntry();
         MapData b = MakeTestData.makeSimpleThreeEntry();
-        assertTrue(Util.deepEquals(a, b, false));
+        assertTrue(Util.deepEquals(a, b, true));
         
         Node n = a.getNodeById(2L);
         n.edgesFrom.remove(0);
         
-        assertFalse(Util.deepEquals(a, b, false));
+        assertFalse(Util.deepEquals(a, b, true));
+    }
+    
+    @Test
+    public void testNodeEquality() {
+        MapData a = MakeTestData.makeSimpleThreeEntry();
+        MapData b = MakeTestData.makeSimpleThreeEntry();
+        
+        assertTrue(Util.deepEquals(a.getNodeById(2L), b.getNodeById(2L), true));
+        assertFalse(Util.deepEquals(a.getNodeById(2L), b.getNodeById(3L), true));
     }
     
 }
