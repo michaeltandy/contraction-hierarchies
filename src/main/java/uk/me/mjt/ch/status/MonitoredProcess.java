7c3beed3ac436ba4bc2770f6f2bf2e3183bfadb0
mtandy <mtandy@mtandy-desktop>
2016-02-18 08:03:05 +0000

    Oops, that wasn't so useful and it broke some unit tests.

--- src/main/java/uk/me/mjt/ch/status/MonitoredProcess.java
+++ src/main/java/uk/me/mjt/ch/status/MonitoredProcess.java
@@ -2,5 +2,5 @@
 package uk.me.mjt.ch.status;
 
 public enum MonitoredProcess {
-    LOAD_NODES, LOAD_WAYS, INDEX_MAP_DATA, VALIDATE_DATA, EXTRA_PROCESS_1, EXTRA_PROCESS_2
+    LOAD_NODES, LOAD_WAYS, INDEX_MAP_DATA, VALIDATE_DATA
 }
